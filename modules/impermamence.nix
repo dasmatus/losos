ea4560f3365a3c6548c7e0ecc5a86873f13961a1
Matus Mastena <Shadiness9530@proton.me>
2026-08-10 19:12:45 +0200

    feat: Reduce repo size

--- modules/impermamence.nix
+++
@@ -1,4 +0,0 @@
-# Superseded by ./impermanence.nix (the filename was misspelled). Kept as a
-# pointer. (The impermanence module attr is `impermanence`, spelled with the
-# second `e`.)
-{ ... }: { }
